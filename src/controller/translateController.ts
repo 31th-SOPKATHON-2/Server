23162ccbf5fd743454aeef9a01e8515bad8ae950
sunseo18 <sunseo1818@gmail.com>
2022-11-20 00:42:24 +0900

    Feat: 번역하기 구현

--- src/controller/translateController.ts
+++ src/controller/translateController.ts
@@ -1,1 +1,30 @@
-export default {};
+import { Request, Response } from 'express';
+import { rm, sc } from '../constants';
+import { fail, success } from '../constants/response';
+import { translateService } from '../service';
+
+const translate = async (req: Request, res: Response) => {
+    try {
+        const input = req.query.input as string;
+        if (!input) return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.BAD_REQUEST));
+
+        //번역 가져오기
+        const output = await translateService.getOutputByInput(input);
+        if (!output) return res.status(sc.NO_CONTENT).send();
+        const data = {
+            input: input,
+            output: output,
+        };
+        return res.status(sc.OK).send(success(sc.OK, '번역 성공', data));
+    } catch (error) {
+        return res
+            .status(sc.INTERNAL_SERVER_ERROR)
+            .send(fail(sc.CREATED, rm.INTERNAL_SERVER_ERROR));
+    }
+};
+
+const translateController = {
+    translate,
+};
+
+export default translateController;
--- src/service/translateService.ts
+++ src/service/translateService.ts
@@ -1,1 +1,23 @@
-export default {};
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+//~ userId로 유저 조회
+const getOutputByInput = async (input: string) => {
+    const data = await prisma.translator.findUnique({
+        where: {
+            input,
+        },
+        select: {
+            result: true,
+            description: true,
+            example: true,
+        },
+    });
+    //단어가 db에 저장이 안되어있을 때
+    if (!data) return null;
+
+    return data;
+};
+
+const translateService = { getOutputByInput };
+export default translateService;
